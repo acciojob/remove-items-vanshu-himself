f9dd840010d2597d14cafae199f8366e7868d667
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-20 22:34:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //your JS code here. If required.
 const selectedval=document.getElementById("colorSelect");
 
-const selectbtn=document.getElementByTagName("input");
-selectbtn.addEventListener("click",remove());
+
 function remove(){
-selectedval.remove(selectedIndex)}
+selectedval.remove(selectedval.value)}
 
 
 
